4eb2fba72f1991d37d652ac9dabfc627524fd42f
Ramazan Küçükkoç <ramazankucukkoc43@gmail.com>
2023-09-14 14:10:51 +0300

    Add project files.

---
+++ WebApı/Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using WebApı.DAL;
+
+namespace WebApı.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        //ElasticClient ve ConnectionSettings kullanımı
+        //için NEST kütüphanesini import ederek devam ediyoruz.
+        private static readonly ConnectionSettings connectionSettings =
+            new ConnectionSettings(new Uri("http://localhost:9200/"))
+            .DefaultIndex("esearchitems")
+            .DefaultMappingFor<Item>(m => m.PropertyName(p => p.Id, "id"));
+
+        private static readonly ElasticClient elasticClient = new ElasticClient(connectionSettings);
+        public ItemsController()
+        {
+            if (!elasticClient.Indices.Exists("esearchitems").Exists)
+            {
+                elasticClient.Indices.Create("esearchitems",
+                    index => index.Map<Item>(
+                        x => x.AutoMap()));
+                elasticClient.Bulk(b => b.Index("esearchitems").IndexMany(ItemService.GetItems()));
+
+            }
+        }
+        [HttpGet]
+        public List<Item> Get()
+        {
+            var response = elasticClient.Search<Item>(i => i
+            .Query(q => q.MatchAll())
+            .PostFilter(f => f.Range(r => r.Field(fi => fi.Price).GreaterThan(1))));
+            List<Item> items = new List<Item>();
+            foreach (var item in response.Documents)
+                items.Add(item);
+            return items;
+        }
+    }
+}
